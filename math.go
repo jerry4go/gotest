881e021d26fc6b1d616569bd7845ed036d0d3dbe
jerry <jerry@gmail.com>
2019-07-10 11:44:09 +0800

    包的管理和使用

---
+++ math.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	mathClass "./myMath"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	fmt.Println(mathClass.Add(1, 1))
+	fmt.Println(mathClass.Sub(1, 1))
+
+}
